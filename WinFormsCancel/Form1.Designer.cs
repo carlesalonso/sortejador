7105c10de7afcfc42d950dcdf2e4980442b5d988
Carles Alonso <carlos.martinez@mataro.epiaedu.cat>
2019-03-31 22:39:44 +0200

    versió amb circlebar

--- WinFormsCancel/Form1.Designer.cs
+++ WinFormsCancel/Form1.Designer.cs
@@ -28,13 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.circularProgressBar1 = new CircularProgressBar.CircularProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(832, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 82);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(832, 597);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 82);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CANCEL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBox1.Location = new System.Drawing.Point(832, 278);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 68);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(371, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(249, 91);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // circularProgressBar1
+            // 
+            this.circularProgressBar1.AnimationFunction = WinFormAnimation.KnownAnimationFunctions.Liner;
+            this.circularProgressBar1.AnimationSpeed = 500;
+            this.circularProgressBar1.BackColor = System.Drawing.Color.Transparent;
+            this.circularProgressBar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Bold);
+            this.circularProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.circularProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.circularProgressBar1.InnerMargin = 2;
+            this.circularProgressBar1.InnerWidth = -1;
+            this.circularProgressBar1.Location = new System.Drawing.Point(241, 251);
+            this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
+            this.circularProgressBar1.Name = "circularProgressBar1";
+            this.circularProgressBar1.OuterColor = System.Drawing.Color.Gray;
+            this.circularProgressBar1.OuterMargin = -25;
+            this.circularProgressBar1.OuterWidth = 26;
+            this.circularProgressBar1.ProgressColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.circularProgressBar1.ProgressWidth = 25;
+            this.circularProgressBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 36F);
+            this.circularProgressBar1.Size = new System.Drawing.Size(352, 335);
+            this.circularProgressBar1.StartAngle = 270;
+            this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
+            this.circularProgressBar1.SubscriptText = "";
+            this.circularProgressBar1.SuperscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
+            this.circularProgressBar1.SuperscriptMargin = new System.Windows.Forms.Padding(10, 35, 0, 0);
+            this.circularProgressBar1.SuperscriptText = "";
+            this.circularProgressBar1.TabIndex = 4;
+            this.circularProgressBar1.TextMargin = new System.Windows.Forms.Padding(8, 8, 0, 0);
+            this.circularProgressBar1.Value = 68;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1267, 863);
+            this.Controls.Add(this.circularProgressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sortejador";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private CircularProgressBar.CircularProgressBar circularProgressBar1;
     }
 }
 
--- WinFormsCancel/Form1.cs
+++ WinFormsCancel/Form1.cs
@@ -7,14 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using System.Security.Cryptography;
 
 namespace WinFormsCancel
 {
     public partial class Form1 : Form
     {
+        CancellationTokenSource cancel;
+        int NombreAlumnes;
         public Form1()
         {
             InitializeComponent();
+            textBox1.Select();
+            label1.Text = "";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            circularProgressBar1.Value = 100;
+            
         }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            UInt32 nombre;
+            if (String.IsNullOrWhiteSpace(textBox1.Text) || 
+                !UInt32.TryParse(textBox1.Text,out nombre) || nombre == 0)
+            {
+                MessageBox.Show("Cal entrar el nombre d'alumnes", "ERROR",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.ResetText();
+                textBox1.Select();
+            }
+            else
+                NombreAlumnes = (int)nombre;
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            cancel = new CancellationTokenSource();
+            var token = cancel.Token;
+            try
+            {
+                button1.Enabled = false;
+                textBox1.Enabled = false;
+                var winner = CalculateRandomWinner();
+                await StartCircleBar(token);
+                label1.Text = winner.ToString();
+                await Task.Delay(TimeSpan.FromSeconds(2));
+
+            }
+            catch(Exception)
+            {
+                
+            }
+            finally 
+            {
+                circularProgressBar1.Value = 100;
+                button1.Enabled = true;
+                textBox1.Enabled = true;
+                textBox1.ResetText();
+                label1.ResetText();
+                textBox1.Select();
+                cancel.Dispose();
+            }
+            
+        }
+
+        private int CalculateRandomWinner()
+        {
+            var provider = new RNGCryptoServiceProvider();
+            var byteArray = new byte[4];
+            provider.GetBytes(byteArray);
+
+            //convert 4 bytes to an integer
+            var randomInteger = BitConverter.ToUInt32(byteArray, 0);
+            return (int)(randomInteger % NombreAlumnes + 1);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cancel.Cancel();
+        }
+
+
+        private  async Task StartCircleBar(CancellationToken token)
+        {
+            for (int i = 100; i >= 0; i--)
+            {
+                token.ThrowIfCancellationRequested();
+                await Task.Delay(TimeSpan.FromSeconds(0.05));
+                circularProgressBar1.Value = i;
+            }
+            await Task.Delay(700);
+        }
+
+        
     }
 }
